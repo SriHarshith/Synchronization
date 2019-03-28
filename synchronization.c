756e0b6addc74842a51c197425173b8e61e16fcb
SriHarshith <34154723+SriHarshith@users.noreply.github.com>
2019-03-28 19:32:43 +0530

    final commit

--- synchronization.c
+++ synchronization.c
@@ -1,35 +1,39 @@
 #include<stdio.h>
-#include<pthread.h>
-#include<semaphore.h>
-#include<string.h>
-#include<stdlib.h>
+#include<pthread.h>//for threads
+#include<semaphore.h>//for semaphore
+#include<string.h>//for using strcmp()
+#include<stdlib.h>//for using  exit() 
 
 void teacher();
 void student1();
 void student2();
 void student3();
-sem_t s;
-int flag1 =0,flag2=0,flag3=0;
-char a[10],b[10],c[10];
+sem_t s;//semaphore variable
+int flag1 =0,flag2=0,flag3=0;//shared  variables
+char a[10],b[10];//userinput strings
+
+//resource variables
 char str1[] = "pen";
 char str2[] = "paper";
 char str3[] = "question paper";
-
+char ex[]="1";//for exiting the program
 
 int main()
 {
-	sem_init(&s,0,1);
-	pthread_t teacher_thread,student_thread;
+	sem_init(&s,0,1);//semaphore initialization
+	pthread_t teacher_thread,student_thread;//thread variables
 	
-	printf("\n\tSelect any 2 resorces to be kept on shared table:\n\n\t\t\t PEN\n \t\t\t PAPER\n\t\t\t QUESTION PAPER\n");
+	printf("\n\tHello Teacher!!");
+	printf("\n\tSelect any 2 Resources to be kept on shared table:\n\n\t\t\t PEN\n \t\t\t PAPER\n\t\t\t QUESTION PAPER\n");
+	printf("\n\t\tPress 1 to exit");
 	
 	while(flag1==0 || flag2==0 || flag3==0)
 	{
-
-	pthread_create(&teacher_thread,NULL,teacher,NULL);
-	pthread_join(teacher_thread,NULL);
 		
-	
+	pthread_create(&teacher_thread,NULL,teacher,NULL);//teacher rhread created 
+	pthread_join(teacher_thread,NULL);//and joined
+		
+		//comparing user input with resorce variables
 	if( ( (strcmp(str1,a)==0 && strcmp(str2,b)==0 ) || (strcmp(str1,b)==0 && strcmp(str2,a)==0 ) ) && flag1==0)
 	{
 		pthread_create(&student_thread,NULL,student1,NULL);
@@ -59,22 +63,33 @@ int main()
 
 void teacher()
 {
-	sem_wait(&s);
+	sem_wait(&s);//executes wait operation on s
 	printf("\n1st Resource : ");
 	gets(a);
-	strlwr(a);
+	strlwr(a);//coverting into lowercase
+	if(strcmp(ex,a)==0)//for exiting 
+	{
+		printf("\n\t\tExiting the progarm................");
+		exit(0);
+	}
+	
 	printf("\n2nd Resorce : ");
 	gets(b);
-	strlwr(b);
-	sem_post(&s);
+	strlwr(b);//converting to lowercase
+	if(strcmp(ex,b)==0)//for exiting
+	{
+		printf("\n\t\tExiting the progarm................");
+		exit(0);
+	}	
+	sem_post(&s);//executes signal operation on s
 	
 }
 
-void student1()//has question paper with him already,need pen and paer to execute
+void student1()//has question paper with him already,need pen and paper to execute
 {
 	sem_wait(&s);
 	printf("Pen and Paper are selected");
-	flag1=1;
+	flag1=1;//updating shared variable
 	printf("\n\nStudent 1 has been allocated all resouces");
 	printf("\nStudent 1 completed the assignment.\n");
 	sem_post(&s);
